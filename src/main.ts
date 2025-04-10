3555b84e945d7e26547bd32a2a1bd227af09a6a4
PrinOrange <prinorange@outlook.com>
2025-04-10 20:49:47 +0800

    chore: format.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { loadRandomInteractionDataset } from "./ngcf/data";
 import { NGCF } from "./ngcf/model";
+import chalk from "chalk";
 
 // ==== Step 1 : Generate matrix of user-item interaction. ====
-console.log("Creating random interaction dataset.");
+console.log(chalk.cyan("Creating random interaction dataset."));
 const numUsers = 100;
 const numItems = 100;
-const minInteractions = 5;
+const minInteractions = 10;
 const maxInteractions = 15;
 const R = loadRandomInteractionDataset(
 	numUsers,
@@ -15,16 +16,16 @@ const R = loadRandomInteractionDataset(
 );
 
 // ==== Step 2 : Load models and input matrix. ====
-console.log("Start loading NGCF model.");
+console.log(chalk.cyan("Start loading NGCF model."));
 const model = new NGCF(numUsers, numItems, R);
 
 // ==== Step 3 : Training model through propagations. ====
-console.log("Training model.");
+console.log(chalk.cyan("Training model."));
 const trainingNum = 10;
 await model.train(trainingNum);
 
 // ==== Step 4 : Generate recommendations according to prediction. ====
-console.log("Generating recommendations.");
+console.log(chalk.cyan("Generating recommendations."));
 const userId = 10;
 const topK = 5;
 const recommendations = model.recommend(userId, topK);
